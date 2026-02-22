76fcda31fbbc8843a3e04a9c0edf829fadbac318
Saty333 <singhsaty.333@gmail.com>
2026-02-23 00:28:40 +0530

    Created basic classes for layers

---
+++ src/main/java/com/capstoneproject/productservice/layer/GodDocument.java
@@ -0,0 +1,15 @@
+package com.capstoneproject.productservice.layer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class GodDocument {
+    private String name;
+    private String id;
+    private String applicationName;
+    private String description;
+}
---
+++ src/main/java/com/capstoneproject/productservice/layer/Module.java
@@ -0,0 +1,13 @@
+package com.capstoneproject.productservice.layer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Module {
+    private String name;
+    private String id;
+    private String description;
+
+}
